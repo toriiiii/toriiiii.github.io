f8fc6e8c952dddc41e428491cf675417605b8f7e
toriiiii <torrance.anonuevo@gmail.com>
2025-09-19 11:32:32 -0400

    add recent projects javascript

--- recent_projects.js
+++ recent_projects.js
@@ -2,3 +2,15 @@ const container = document.getElementById('recent-projects-container');
 const loadBtn = document.getElementById('load-more-btn');
 let loadSize = 3;
 
+function showMostRecentProjects() {
+    projects = document.getElementsByClassName("col s12 m6 l4");
+    for (let i = 0; i < projects.length; i++) {
+        if (i >= loadSize) {
+            break;
+        }
+
+        card = projects[i].cloneNode(true);
+        container.appendChild(card);
+    }
+}
+
