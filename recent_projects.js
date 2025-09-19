af79a1524fb89a1e3426c4b9e5a68999f28f75af
toriiiii <torrance.anonuevo@gmail.com>
2025-09-19 14:27:50 -0400

    remove line that erased everything

--- recent_projects.js
+++ recent_projects.js
@@ -55,9 +55,9 @@ const projects = [
 ];
 
 function showMostRecentProjects() {
+    container.innerHTML = "";
     for (let i = 0; i < projects.length; i++) {
         const project = projects[i];
-        container.innerHTML = "";
         if (i < loadSize) {
             container.innerHTML += `
                 <div class="col s12 m6 l4">
